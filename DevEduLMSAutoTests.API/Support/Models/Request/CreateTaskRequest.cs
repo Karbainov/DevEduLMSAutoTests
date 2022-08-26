d11cff49ac8b0ae0ec8e938d47ed17335fc664d9
baikovIlya <baikusha23@gmail.com>
2022-08-26 23:05:18 +0300

    Add Request models

---
+++ DevEduLMSAutoTests.API/Support/Models/Request/CreateTaskRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace DevEduLMSAutoTests.API.Support.Models.Request
+{
+    public class CreateTaskRequest
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("links")]
+        public string Links { get; set; }
+
+        [JsonPropertyName("isRequired")]
+        public bool IsRequired { get; set; }
+
+        [JsonPropertyName("courseIds")]
+        public int[] courseIds { get; set; }
+    }
+}
