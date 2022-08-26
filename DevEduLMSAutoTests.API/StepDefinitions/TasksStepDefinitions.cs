ad1bcddde3c6095b61d2cff141226d518c463d5b
baikovIlya <baikusha23@gmail.com>
2022-08-26 23:07:30 +0300

    Add Step Definitions for "Tasks"

---
+++ DevEduLMSAutoTests.API/StepDefinitions/TasksStepDefinitions.cs
@@ -0,0 +1,63 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace DevEduLMSAutoTests.API.StepDefinitions
+{
+    [Binding]
+    public class TasksStepDefinitions
+    {
+        [Given(@"register new users")]
+        public void GivenRegisterNewUsers(Table table)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"authorize users")]
+        public void GivenAuthorizeUsers(Table table)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"manager add roles to users")]
+        public void GivenManagerAddRolesToUsers()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"manager create new group")]
+        public void GivenManagerCreateNewGroup(Table table)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"manager add users to group")]
+        public void GivenManagerAddUsersToGroup()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"methodist create new task")]
+        public void GivenMethodistCreateNewTask(Table table)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"methodist update task")]
+        public void GivenMethodistUpdateTask(Table table)
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"teacher post task")]
+        public void WhenTeacherPostTask()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"student should sees task")]
+        public void ThenStudentShouldSeesTask()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
