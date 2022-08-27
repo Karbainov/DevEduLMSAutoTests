879bec7e93c4bd2e717ae7eef6b51ec15303792a
Anastasiia <vostrova_1994@mail.ru>
2022-08-27 16:06:36 +0300

    Add response, request model

---
+++ DevEduLMSAutoTests.API/Support/Models/Request/AddTopicsRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace DevEduLMSAutoTests.API.Support.Models.Request
+{
+    public class AddTopicsRequest
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("duration")]
+        public int Duration { get; set; }
+    }
+}
---
+++ DevEduLMSAutoTests.API/Support/Models/Response/AddTopicsResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace DevEduLMSAutoTests.API.Support.Models.Response
+{
+    public class AddTopicsResponse
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("duration")]
+        public int Duration { get; set; }
+    }
+}
