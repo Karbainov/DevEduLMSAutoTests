41bf14693486b569d5f71158e32a3a80e5c41583
Valeriya Puzikova <happyvaleriya@gmail.com>
2022-08-27 13:46:53 +0300

    Add request models

---
+++ DevEduLMSAutoTests.API/Support/Models/Request/AddHomeworkByStudentRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+
+namespace DevEduLMSAutoTests.API.Support.Models.Request
+{
+    public class AddHomeworkByStudentRequest
+    {
+        [JsonPropertyName("homeworkId")]
+        public int HomeworkId { get; set; }
+
+        [JsonPropertyName("answer")]
+        public string Answer { get; set; }
+    }
+}
