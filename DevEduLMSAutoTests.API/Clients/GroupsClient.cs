701b7e65a54e25f66324a4463ffed1e398a1a692
baikovIlya <baikusha23@gmail.com>
2022-08-27 15:13:40 +0300

    Add clients

---
+++ DevEduLMSAutoTests.API/Clients/GroupsClient.cs
@@ -0,0 +1,33 @@
+﻿namespace DevEduLMSAutoTests.API.Clients
+{
+    public class GroupsClient
+    {
+        public GetAllGroupsResponse CreateNewGroup(CreateGroupRequest newGroup, string managerToken)
+        {
+            string json = JsonSerializer.Serialize(newGroup);
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", managerToken);
+            HttpRequestMessage message = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new System.Uri(Urls.Groups),
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
+            HttpResponseMessage responseMessage = client.Send(message);
+            GetAllGroupsResponse response = JsonSerializer.Deserialize<GetAllGroupsResponse>
+                (responseMessage.Content.ReadAsStringAsync().Result)!;
+            return response;
+        }
+
+        public void AddUserToGroup(int groupId, int userId, string role, string managerToken)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", managerToken);
+            HttpRequestMessage message = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new System.Uri($"Urls.Groups/{groupId}/user/{userId}/role/{role}"),
+            };
+        }
+    }
+}
---
+++ DevEduLMSAutoTests.API/Clients/TasksClient.cs
@@ -0,0 +1,54 @@
+﻿namespace DevEduLMSAutoTests.API.Clients
+{
+    public class TasksClient
+    {
+        public CreateNewTaskResponse AddTaskByMethodist(CreateTaskByMethodistRequest newTask, string methodistToken)
+        {
+            string json = JsonSerializer.Serialize(newTask);
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", methodistToken);
+            HttpRequestMessage message = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new System.Uri($"{Urls.Tasks}/methodist"),
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
+            HttpResponseMessage response = client.Send(message);
+            CreateNewTaskResponse responseTask = JsonSerializer.Deserialize<CreateNewTaskResponse>
+                (response.Content.ReadAsStringAsync().Result)!;
+            return responseTask;
+        }
+
+        public CreateNewTaskResponse UpdateTask(UpdateTaskRequest newTask, int taskId, string token)
+        {
+            string json = JsonSerializer.Serialize(newTask);
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            HttpRequestMessage message = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new System.Uri($"{Urls.Tasks}/{taskId}"),
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
+            HttpResponseMessage response = client.Send(message);
+            CreateNewTaskResponse responseTask = JsonSerializer.Deserialize<CreateNewTaskResponse>
+                (response.Content.ReadAsStringAsync().Result)!;
+            return responseTask;
+        }
+
+        public List<CreateNewTaskResponse> GetTasksByGroupId(int groupId, string teacherToken)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", teacherToken);
+            HttpRequestMessage message = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new System.Uri($"{Urls.Tasks}/by-group/{groupId}")
+            };
+            HttpResponseMessage response = client.Send(message);
+            List<CreateNewTaskResponse> responseTasks = JsonSerializer.Deserialize<List<CreateNewTaskResponse>>
+                (response.Content.ReadAsStringAsync().Result)!;
+            return responseTasks;
+        }
+    }
+}
