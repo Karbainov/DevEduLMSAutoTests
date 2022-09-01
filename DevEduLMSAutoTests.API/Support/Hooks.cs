5c3eea8887384fe393a303ed7160a63287f62e8f
Valeriya Puzikova <happyvaleriya@gmail.com>
2022-09-01 17:30:06 +0300

    Add few files from Ilya branch

---
+++ DevEduLMSAutoTests.API/Support/Hooks.cs
@@ -0,0 +1,24 @@
+﻿namespace DevEduLMSAutoTests.API.Support
+{
+    [Binding]
+    public sealed class Hooks
+    {
+        private ClearBase _clearTables;
+        public Hooks()
+        {
+            _clearTables = new ClearBase();
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            _clearTables.ClearDB();
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            _clearTables.ClearDB();
+        }
+    }
+}
