1938b79b2cfb12249447d941ff1f758c0ea1d376
Valeriya Puzikova <happyvaleriya@gmail.com>
2022-09-06 16:52:21 +0300

    Add scenario and step definition

---
+++ AutoTestsSelenium/Features/CreateLessonDraftAndPublish.feature
@@ -0,0 +1,11 @@
+﻿Feature: CreateLessonDraftAndPublish
+
+A short summary of the feature
+
+@lesson
+Scenario: Teacher create a lesson. Save as draft and publish it
+	Given Open the browser
+	And Open devedu page
+	Given Sign in as a teacher
+	And Click on Create lesson
+
---
+++ AutoTestsSelenium/StepDefinitions/CreateLessonDraftAndPublishStepDefinitions.cs
@@ -0,0 +1,33 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace AutoTestsSelenium.StepDefinitions
+{
+    [Binding]
+    public class CreateLessonDraftAndPublishStepDefinitions
+    {
+        [Given(@"Open the browser")]
+        public void GivenOpenTheBrowser()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"Open dev-edu page")]
+        public void GivenOpenDeveduPage()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"Sign in as a teacher")]
+        public void GivenSignInAsATeacher()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"Click on Create lesson")]
+        public void GivenClickOnCreateLesson()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
