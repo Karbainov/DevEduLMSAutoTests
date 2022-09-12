91c127c4b4f5579238c618e065aea00b31df118f
MaksimK <el_makso@mail.ru>
2022-09-12 12:54:50 +0300

    Add example

---
+++ AutoTestsSelenium/PageObjects/LoginPage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTestsSelenium.PageObjects
+{
+    public class LoginPage:AbstractPage
+    {
+        public IWebElement TextBoxEmail {get; set;}
+        public IWebElement TextBoxPassword {get; set;}
+        public IWebElement ButtonEnter {get; set;}
+        public IWebElement TextIncorrectEmailFormat {
+            get
+            {
+                return _driver.FindElement(By.XPath($"/html/body/div/div/main/div[1]/form/div[2]"));
+            } 
+            private set
+            {
+
+            }
+        }
+
+        public LoginPage(IWebDriver driver):base(driver)
+        {
+            TextBoxEmail = _driver.FindElement(By.XPath($"//*[@id='root']/div/main/div[1]/form/div[1]/input"));
+            TextBoxPassword = _driver.FindElement(By.XPath($"/html/body/div/div/main/div[1]/form/div[2]/input"));
+            ButtonEnter = _driver.FindElement(By.XPath($"//button[text()='Войти']"));
+        }
+
+        public void EnterEmail(string email)
+        {
+            TextBoxEmail.SendKeys(email);
+        }
+
+        public void EnterPassword(string password)
+        {
+            TextBoxPassword.Clear();
+            TextBoxPassword.SendKeys(password);
+        }
+
+        public void ClickEnterButton()
+        {
+            ButtonEnter.Click();
+        }
+    }
+}
