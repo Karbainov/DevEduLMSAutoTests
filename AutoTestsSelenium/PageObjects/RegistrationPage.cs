b7c770bb56c4c07c43811abf6a24fd5598e9699b
Milana Maksina <m_maksinaa@mail.ru>
2022-09-13 21:48:18 +0300

    feat: add registration page and its properties

---
+++ AutoTestsSelenium/PageObjects/RegistrationPage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTestsSelenium.PageObjects
+{
+    internal class RegistrationPage : AbstractPage
+    {
+        public IWebElement TextBoxLastName { get; set; }
+        public IWebElement TextBoxName { get; set; }
+        public IWebElement TextBoxPatronymic { get; set; }
+        public IWebElement TextBoxBirthDate { get; set; }
+        public IWebElement TextBoxPassword { get; set; }
+        public IWebElement TextBoxRepeatPassword { get; set; }
+        public IWebElement TextBoxEmail { get; set; }
+        public IWebElement CheckBoxConfirmRules { get; set; }
+        public IWebElement ButtonEnter { get; set; }
+        public IWebElement ButtonRegistrate { get; set; }
+        public IWebElement ButtonCancelRegistration { get; set; }
+
+        public RegistrationPage(IWebDriver driver) : base(driver)
+        {
+            
+        }
+    }
+}
