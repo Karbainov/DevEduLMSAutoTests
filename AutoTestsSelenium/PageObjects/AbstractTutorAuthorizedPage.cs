cb815e7c76ad6698ef14da4fcff919a9ed2da793
baikovIlya <baikusha23@gmail.com>
2022-09-14 19:14:00 +0300

    Add AbstractTutorAuthorizedPage

---
+++ AutoTestsSelenium/PageObjects/AbstractTutorAuthorizedPage.cs
@@ -0,0 +1,46 @@
+﻿namespace AutoTestsSelenium.PageObjects
+{
+    public abstract class AbstractTutorAuthorizedPage : AbstractAuthorizedPage
+    {
+        public IWebElement ButtonLessonsSideBar => _driver.FindElement(By.XPath($"//*[text()='Занятия']/.."));
+        public IWebElement ButtonHomeworksSideBar => _driver.FindElement(By.XPath($"//*[text()='Домашние задания']/.."));
+        public IWebElement ButtonAddHomewrksSideBar => _driver.FindElement(By.XPath($"//*[text()='Выдача заданий']/.."));
+        public IWebElement ButtonCheckHomeworksSideBar => _driver.FindElement(By.XPath($"//*[text()='Проверка заданий']/.."));
+        public IWebElement ButtonGeneralProgressSideBar => _driver.FindElement(By.XPath($"//*[text()='Общая успеваемость']/.."));
+        public IWebElement ButtonJournalSideBar => _driver.FindElement(By.XPath($"//*[text()='Журнал']/.."));
+
+        protected AbstractTutorAuthorizedPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public void ClickLessonsButton()
+        {
+            ButtonLessonsSideBar.Click();
+        }
+
+        public void ClickHomeworksButton()
+        {
+            ButtonHomeworksSideBar.Click();
+        }
+
+        public void ClickAddHomeworksButton()
+        {
+            ButtonAddHomewrksSideBar.Click();
+        }
+
+        public void ClickCheckHomeworksButton()
+        {
+            ButtonCheckHomeworksSideBar.Click();
+        }
+
+        public void ClickGeneralProgressButton()
+        {
+            ButtonGeneralProgressSideBar.Click();
+        }
+
+        public void ClickJournalButton()
+        {
+            ButtonJournalSideBar.Click();
+        }
+    }
+}
