e816b900a9d0b5366cd837d46b0a53edec7f0cd6
baikovIlya <baikusha23@gmail.com>
2022-09-15 16:06:52 +0300

    Add GroupsManagerAuthorizedPage

---
+++ AutoTestsSelenium/PageObjects/GroupsManagerAuthorizedPage.cs
@@ -0,0 +1,36 @@
+﻿namespace AutoTestsSelenium.PageObjects
+{
+    public class GroupsManagerAuthorizedPage : AbstractManagerAuthorizedPage
+    {
+        public const string PageUrl = $"{Urls.Host}/groups";
+        public GroupsManagerAuthorizedPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public override void OpenThisPage()
+        {
+            _driver.Navigate().GoToUrl(PageUrl);
+        }
+
+        public IWebElement DesiredGroup(string groupName)
+        {
+            string xpath = $"//*[text()='{groupName}']/..";
+            return _driver.FindElement(By.XPath(xpath));
+        }
+
+        public void ChooseGroup(string groupName)
+        {
+            DesiredGroup(groupName).Click();
+        }
+
+        public void ClickButtonEdit()
+        {
+            _driver.FindElement(By.XPath($"//a[text()='Редактировать']")).Click();
+        }
+
+        public void ClickButtonEditStudentsList()
+        {
+            _driver.FindElement(By.XPath($"//a[text()='Редактировать список группы']")).Click();
+        }
+    }
+}
