1a1d937c4b403d6f51fee6d48d8805b3ed8be274
baikovIlya <baikusha23@gmail.com>
2022-09-15 17:33:37 +0300

    Add teachers pages

---
+++ AutoTestsSelenium/PageObjects/LessonsTeacherAuthorizedPage.cs
@@ -0,0 +1,22 @@
+﻿namespace AutoTestsSelenium.PageObjects
+{
+    public class LessonsTeacherAuthorizedPage : AbstractTeacherAuthorizedPage
+    {
+        public const string PageUrl = $"{Urls.Host}/lessons";
+
+        public LessonsTeacherAuthorizedPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public override void OpenThisPage()
+        {
+            _driver.Navigate().GoToUrl(PageUrl);
+        }
+
+        public IWebElement DesiredGroup(string groupName)
+        {
+            string xpath = $"//*[text()='{groupName}']/..";
+            return _driver.FindElement(By.XPath(xpath));
+        }
+    }
+}
