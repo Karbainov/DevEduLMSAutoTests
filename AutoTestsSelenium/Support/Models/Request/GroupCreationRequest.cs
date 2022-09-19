a30537eadfa68584a875e9803fdb2dfb83c36ad7
Valeriya Puzikova <happyvaleriya@gmail.com>
2022-09-19 06:12:15 +0300

    Add group creation model

---
+++ AutoTestsSelenium/Support/Models/Request/GroupCreationRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace AutoTestsSelenium.Support.Models.Request
+{
+    public class GroupCreationRequest
+    {
+        public string Name { get; set; }
+        public string CourseName { get; set; }
+        public string TeacherFirstName { get; set; }
+        public string TeacherLastName { get; set; }
+        public string TutorFirstName { get; set; }
+        public string TutorLastName { get; set; }
+    }
+}
