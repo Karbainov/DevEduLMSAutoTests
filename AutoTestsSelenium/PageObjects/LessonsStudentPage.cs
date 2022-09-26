fbc25e62fa15b9fec5befc6e94341ef8fcae3ebc
baikovIlya <baikusha23@gmail.com>
2022-09-26 18:14:35 +0300

    fix: all CRLF

--- AutoTestsSelenium/PageObjects/LessonsStudentPage.cs
+++ AutoTestsSelenium/PageObjects/LessonsStudentPage.cs
@@ -2,7 +2,8 @@
 {
     public class LessonsStudentPage : AbstractStudentAuthorizedPage
     {
-        private const string PageUrl = $"{Urls.Host}/lessons";
+        private const string PageUrl = $"{Urls.Host}/lessons";
+
         public List<IWebElement> StudentGroups => _driver.FindElements(By.XPath($"//*[@class='tab-container']")).ToList();
 
         public LessonsStudentPage()
